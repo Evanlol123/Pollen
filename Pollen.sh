ad20f0277dfd2d8a1bd25e6d6fdf9a21bc53ebf5
Evanlol123 <116678253+Evanlol123@users.noreply.github.com>
2024-04-02 11:41:16 -0400

    Update Pollen.sh

--- Pollen.sh
+++ Pollen.sh
@@ -20,17 +20,13 @@ echo "May Ultrablue Rest in Peace, o7"
 
 sleep 1
 
-# Prompt the user to copy and paste the JSON content
 echo "Please copy and paste your JSON content below:"
 read -r json_content
 
-# Create the necessary directories
 mkdir -p /tmp/overlay/etc/opt/chrome/policies/managed
 
-# Write the JSON content to the policy file
 echo "$json_content" > /tmp/overlay/etc/opt/chrome/policies/managed/policy.json
 
-# Bind mount the overlay directory to /etc
 cp -a -L /etc/* /tmp/overlay/etc 2> /dev/null
 mount --bind /tmp/overlay/etc /etc
 
